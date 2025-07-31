d5fa5f9060cd925e6556662de9a65a652be2a204
enesdolgun33 <enesdolgun33@gmail.com>
2025-07-31 14:40:07 +0300

    chptr13

--- 7.course-app/src/App.jsx
+++ 7.course-app/src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from './header'
+import { courses } from './Data'
+import Course from './Course'
+
 
 function App() {
 
   return (
     <div>
       <Header />
-      
+
+      {
+        courses?.map((course)=>(
+          <Course key={course.id} course={course} />
+
+        ))
+      }
+
     </div>
     
   )
---
+++ 7.course-app/src/Data.jsx
@@ -0,0 +1,42 @@
+import Csharp from './images/csharp.jpg';
+import Java from './images/java.jpg';
+import Sql from './images/sql.jpg';
+import Web from './images/web.jpg';
+
+export const courses = [
+    {
+        id : 123,
+        title : "C# Programlama Dili",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio perferendis dolores error deleniti voluptatibus necessitatibus velit pariatur natus quo quaerat.",
+        price : 120,
+        link : "https://www.udemy.com/course/c-sharp-programlama-dili-her-seviyeye-hitap-eden-egitim-seti/",
+        image : Csharp
+    },
+
+    {
+        id : 124,
+        title : "Java Programlama Dili",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio perferendis dolores error deleniti voluptatibus necessitatibus velit pariatur natus quo quaerat.",
+        price : 130,
+        link : "https://www.udemy.com/course/java-programlama-dili-her-seviyeye-uygun-egitim-seti/",
+        image : Java
+    },
+
+    {
+        id : 125,
+        title : "Sql Server Eğitimi",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio perferendis dolores error deleniti voluptatibus necessitatibus velit pariatur natus quo quaerat.",
+        price : 135,
+        link : "https://www.udemy.com/course/uygulamarla-sql-server-ogreniyorum-adan-zye/",
+        image : Sql
+    },
+    
+    {
+        id : 126,
+        title : "Modern Web Geliştirme Eğitimi",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio perferendis dolores error deleniti voluptatibus necessitatibus velit pariatur natus quo quaerat.",
+        price : 150,
+        link : "https://www.udemy.com/course/modern-web-gelistirme-kursu-sifirdan-ileri-seviyeye/",
+        image : Web
+    },
+]
