fce0360e75f32fa0f55f3763ca653afc47a0c106
enesdolgun33 <enesdolgun33@gmail.com>
2025-08-05 18:48:36 +0300

    chptr34
    iç içe routed yapısı

--- 12.router-dom/src/App.jsx
+++ 12.router-dom/src/App.jsx
@@ -5,7 +5,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Products from './pages/Products';
-
+import EmployeeAbout from './pages/EmployeeAbout';
+import CompanyAbout from './pages/CompanyAbout';
 
 // Routes Route
 
@@ -16,7 +17,10 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
+        <Route path='/about' element={<About/>}>
+          <Route path='employee' element={<EmployeeAbout/>}/>
+          <Route path='company' element={<CompanyAbout/>}/>
+        </Route>
         <Route path='/contact' element={<Contact/>}></Route>
         <Route path='/products' element={<Products/>}></Route>
         <Route path='*' element={<NotFoundPage/>}></Route>
--- 12.router-dom/src/pages/About.jsx
+++ 12.router-dom/src/pages/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1>About</h1>
+      <hr />
+      <Link className='link2' to="employee">Çalışanlar Hakkında</Link>
+      <Link className='link2' to="company">Şirket Hakkında</Link>
+      {/* buradaki to kısımlarının başına / konulmaz konulursa içindeki değeri kök yapar child olarak almaz */}
+      <Outlet/>
+    </div>
   )
 }
 
