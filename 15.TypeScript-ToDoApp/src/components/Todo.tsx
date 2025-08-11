93ebb7a9722b43afc3cefc58e3c483dd4b6ea7b0
enesdolgun33 <enesdolgun33@gmail.com>
2025-08-11 17:35:39 +0300

    chptr47
    Redux + Ts to do app

--- 15.TypeScript-ToDoApp/src/components/Todo.tsx
+++ 15.TypeScript-ToDoApp/src/components/Todo.tsx
@@ -2,11 +2,21 @@ import { CiCircleRemove } from "react-icons/ci";
 import { FaCheck } from "react-icons/fa";
 import { CiEdit } from "react-icons/ci";
 import '../css/Todo.css'
+import type { TodoType } from "../types/Types";
 
-function Todo() {
+interface TodoProps {
+    todoProps: TodoType
+}
+
+function Todo({ todoProps }: TodoProps) {
+
+    const { id, content } = todoProps;
+
     return (
         <div className="todo-div">
-            <div>Ben ilk todoyum</div>
+            <div>
+                {content}
+            </div>
             <div>
                 <CiCircleRemove className="icons" />
                 <CiEdit className="icons" />
