98e9c1bd301bce9891af034e5624ca1a9e16445f
enesdolgun33 <enesdolgun33@gmail.com>
2025-08-11 19:57:07 +0300

    chptr48
    todo app #4

--- 15.TypeScript-ToDoApp/src/components/Todo.tsx
+++ 15.TypeScript-ToDoApp/src/components/Todo.tsx
@@ -3,6 +3,8 @@ import { FaCheck } from "react-icons/fa";
 import { CiEdit } from "react-icons/ci";
 import '../css/Todo.css'
 import type { TodoType } from "../types/Types";
+import { useDispatch } from "react-redux";
+import { removeTodoById } from "../redux/todoSlice";
 
 interface TodoProps {
     todoProps: TodoType
@@ -12,13 +14,19 @@ function Todo({ todoProps }: TodoProps) {
 
     const { id, content } = todoProps;
 
+
+    const dispatch = useDispatch();
+    const handleRemoveTodo = () => {
+        dispatch(removeTodoById(id))
+    }
+
     return (
         <div className="todo-div">
             <div>
                 {content}
             </div>
             <div>
-                <CiCircleRemove className="icons" />
+                <CiCircleRemove onClick={handleRemoveTodo} className="icons" />
                 <CiEdit className="icons" />
             </div>
         </div>
